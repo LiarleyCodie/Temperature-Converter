6658f82f63736c0269cc8a3234ee59d476cf3b65
liarleycodie <liarley.personal@gmail.com>
2023-04-04 13:32:27 -0300

    stage: :corn: function that prints texts with user instructions and conditions that evaluates user entries created

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cctype>
+#include <conio.h>
+
 #ifdef _WIN32
 #include <windows.h>
 #else
@@ -8,21 +11,32 @@
 class Converter
 {
 public:
-    void clearConsole()
-    {
-        #ifdef _WIN32
-        system("cls");
-        #else
-        std::cout << "\033[2J\033[1;1H";
-        #endif // _WIN32
-    }
-    void printTitle()
-    {
-        std::cout << "=====================================\n";
-        std::cout << "        Temperature Converter        \n";
-        std::cout << "=====================================\n";
-        std::cout << std::endl;
-    };
+  void clearConsole()
+  {
+      #ifdef _WIN32
+      system("cls");
+      #else
+      std::cout << "\033[2J\033[1;1H";
+      #endif // _WIN32
+  }
+  void printTitle()
+  {
+      std::cout << "=====================================\n";
+      std::cout << "        Temperature Converter        \n";
+      std::cout << "=====================================\n";
+      std::cout << std::endl;
+  };
+  void printTemperatureType()
+  {
+    std::cout << "Which temperature do you want to converter?\n";
+    std::cout << "[C]: Celsius\n";
+    std::cout << "[K]: Kelvin\n";
+    std::cout << "[F]: Fahrenheit\n";
+    std::cout << "Type a letter: ";
+  }
+  class Formulas
+  {
+  public:
     double CelsiusToFahrenheit(double value)
     {
         return (9/5) * value + 32;
@@ -47,27 +61,46 @@ public:
     {
         return (9/5) * (value - 273.15) + 32;
     };
-
+  };
 };
+
+
 
 int main()
 {
-    Converter converter;
-    char temperatureFormula;
+  Converter converter;
+  Converter::Formulas formulas;
+  char temperatureFormula;
+  bool waitingChooseTemperature {true};
+  char c;
 
+  while (waitingChooseTemperature)
+  {
     converter.printTitle();
-
-    std::cout << "Which temperature do you want to converter?\n";
-    std::cout << "[C]: Celsius\n";
-    std::cout << "[K]: Kelvin\n";
-    std::cout << "[F]: Fahrenheit\n";
-    std::cout << "Type a letter: ";
-
+    converter.printTemperatureType();
     std::cin >> temperatureFormula;
+    c = toupper(temperatureFormula);
 
-    converter.clearConsole();
+    switch (c)
+    {
+      case 'C':
+        waitingChooseTemperature = false;
+      break;
+      case 'F':
+        waitingChooseTemperature = false;
+      break;
+      case 'K':
+        waitingChooseTemperature = false;
+      break;
+      default:
+        std::cout << "Wrong Entry!";
+        std::cout << "\nPress any key to continue...";
+        while (!kbhit()) {};
+        converter.clearConsole();
+    }
+  }
 
-    return 0;
+  return 0;
 }
 
 
